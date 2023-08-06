167592328f520245e6f775df08ada9981e7e76be
JP <jay.patel.py@gmail.com>
2023-08-06 10:15:47 -0600

    Added sidebar for mobile view

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,13 +2,22 @@
 
 import { Menu } from "lucide-react";
 
+import Sidebar from "@/components/sidebar";
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const MobileSidebar = () => {
   return (
-    <Button variant="ghost" size="icon" className="md:hidden ">
-      <Menu />
-    </Button>
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden ">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
   )
 }
 

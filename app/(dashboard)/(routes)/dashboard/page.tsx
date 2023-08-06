d56bee53752c753b6386d2ba75598aa1726fd4f1
JP <jay.patel.py@gmail.com>
2023-08-06 11:15:04 -0600

    Added card component

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,35 @@
+"use client";
+
+import { MessageSquare } from "lucide-react";
+
+import { Card } from "@/components/ui/card";
+
+const tools = [
+  {
+    lable: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+];
+
 const DashboardPage = () => {
   return (
-    <div>Dashboard Page
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl text-center font-bold">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+
+        ))}
+      </div>
     </div>
   )
 }

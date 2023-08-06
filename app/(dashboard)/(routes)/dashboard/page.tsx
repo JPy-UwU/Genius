a6b2523bff140caa76e4421c1eeb77c8cdc1b6a8
JP <jay.patel.py@gmail.com>
2023-08-06 14:19:24 -0600

    added link cards to dashboard

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { MessageSquare } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ArrowRight, Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
 
 import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 const tools = [
   {
@@ -12,9 +14,38 @@ const tools = [
     color: "text-violet-500",
     bgColor: "bg-violet-500/10",
   },
+  {
+    lable: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+  },
+  {
+    lable: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10",
+  },
+  {
+    lable: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10",
+  },
+  {
+    lable: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+    bgColor: "bg-green -700/10",
+  },
 ];
 
 const DashboardPage = () => {
+  const router = useRouter();
   return (
     <div>
       <div className="mb-8 space-y-4">
@@ -27,7 +58,21 @@ const DashboardPage = () => {
       </div>
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
         {tools.map((tool) => (
-
+          <Card 
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className="p-4 border-black-5 flex items-center justify-between hover:shadow-md transition cursor-pointer"  
+          >
+            <div className="flex items-center gap-x-4">
+              <div className={cn("p-2 rounded-md w-fit", tool.bgColor)}>
+                <tool.icon className={cn("w-8 h-8", tool.color)} />
+              </div>
+              <div className="font-semibold">
+                {tool.lable}
+              </div>
+            </div>
+            <ArrowRight className="w-5 h-5" />
+          </Card>
         ))}
       </div>
     </div>

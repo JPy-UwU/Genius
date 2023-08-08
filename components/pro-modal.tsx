a4b77382eaeda50d83232679b6e2abb5430bda94
JP <jay.patel.py@gmail.com>
2023-08-07 23:52:09 -0600

    connected to zustand store

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 const ProModal = () => {
+  const proModal = useProModal();
+
   return (
-    <Dialog open>
+    <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">

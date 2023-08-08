2f7ab97ec8ec764c71d0449bc98f756ae1f7d3b1
JP <jay.patel.py@gmail.com>
2023-08-07 23:50:07 -0600

    created a basic modal dialog

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
 const ProModal = () => {
   return (
-    <div>ProModal</div>
+    <Dialog open>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+            Upgrade to Genius
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
 

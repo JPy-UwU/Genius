a037922e06b3c01067ea30742be9998b1df31f22
JP <jay.patel.py@gmail.com>
2023-08-08 19:25:52 -0600

    added loading state

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -94,6 +94,7 @@ const ProModal = () => {
         </DialogHeader>
         <DialogFooter>
           <Button
+            disabled={loading}
             onClick={onSubscribe}
             size="lg"
             variant="premium"
